51cedf8f8309503cc1c2ec525c71b17c4720b6c8
Aadarsh675 <76542875+Aadarsh675@users.noreply.github.com>
2021-10-24 09:47:55 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
-game_status = "";
-
 function setup(){
-var canvas =  createCanvas(700,600);
-canvas.parent('canvas');
+    var canvas =  createCanvas(700,600);
+    canvas.parent('canvas');
+    video = createCapture(VIDEO);
+    video.size(700, 600);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded() {
+  console.log('Model Loaded Successfully');
+}
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        leftWristY = results[0].pose.leftWrist.y;
+        rightWristY = results[0].pose.rightWrist.y;
+    }
+}
+function draw() {
+    image(video, 0, 0, 700, 600);
 }
